68d17cacaed1874e029da2dcc4f6f508a98c5d9d
Dil <126651350+SAGAR1215g@users.noreply.github.com>
2023-07-06 13:58:35 +0530

    Update thumbnails.py

--- AarohiX/utils/thumbnails.py
+++ AarohiX/utils/thumbnails.py
@@ -124,7 +124,7 @@ async def gen_thumb(videoid):
 
             image4 = ImageDraw.Draw(image2)
             image4.text((10, 10), "DIL[ISHU] x MUSIC", fill="white", font = font1, align ="left")
-            image4.text((10, 10), "Ishu[Wife♡Hubby]Dil", fill="white", font = font1, align ="right")
+            image4.text((50, 50), "Ishu[Wife♡Hubby]Dil", fill="white", font = font1, align ="right")
             image4.text((670, 150), "NOW PLAYING", fill="white", font = font2, stroke_width=2, stroke_fill="red", align ="left") 
 
             # title
